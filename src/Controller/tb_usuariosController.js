0cbea1ae9afc54af28618c300d00346997c7d244
bruhsvicente <brutronica931@gmail.com>
2024-10-26 20:20:19 -0300

    Token do Usuário (ADM) adicionado

--- src/Controller/tb_usuariosController.js
+++ src/Controller/tb_usuariosController.js
@@ -1,17 +1,16 @@
 import * as bd from '../Repository/tb_usuariosRepository.js'
 import { validarUsuarios } from '../Validation/usuariosValidation.js'
+import { gerarToken } from "../utils/jwt.js"
 
-import { autenticar, gerarToken } from "../utils/jwt.js"
 import { Router } from 'express'
 const endpoints = Router()
 
 
-endpoints.post('/tdl/usuarios/inserir/', async (req, resp) => {
+endpoints.post('/tdl/usuarios/entrar', async (req, resp) => {
     try {
-        let usuario = req.body
-
-        validarUsuarios(usuario)
-        await bd.inserirUsuario(usuario)
+        let pessoa = req.body
+        validarUsuarios(pessoa)
+        let usuario = await bd.validarUsuario(pessoa)
 
         if (usuario === null) {
             resp.send({ erro: "Usuário ou senha incorreto(s)." })
@@ -30,41 +29,5 @@ endpoints.post('/tdl/usuarios/inserir/', async (req, resp) => {
     }
 })
 
-// Usuário
-
-endpoints.get('/tdl/usuarios/consulta', autenticar, async (req, resp) => {
-    try {
-        let pessoa = req.user
-        let usuario = await bd.validarUsuario(pessoa)
-
-        if (usuario === null) {
-            resp.send({ erro: "Usuário ou senha incorreto(s)." })
-        }
-        else {
-            resp.send(usuario)
-        }
-    }
-    catch (err) {
-        resp.status(400).send({
-            erro: err.message
-        })
-    }
-})
-
-// Nome Usuário
-
-endpoints.get('/tdl/usuarios/consulta/:id', autenticar, async (req, resp) => {
-    try {
-
-        let id = req.params.id
-        let registros = await bd.consultaNomeUsuario(id)
-        resp.send(registros)
-    }
-    catch (err) {
-        resp.status(400).send({
-            erro: err.message
-        })
-    }
-})
 
 export default endpoints;
--- src/Repository/tb_usuariosRepository.js
+++ src/Repository/tb_usuariosRepository.js
@@ -1,15 +1,5 @@
+
 import con from "./connection.js";
-
-export async function inserirUsuario(usuario) {
-    let comando = `
-    insert tb_usuarios 	(nome, senha) 
-    values              (?, ?);
-    `
-
-    let resposta = await con.query(comando, [usuario.nome, usuario.senha])
-    let info = resposta[0]
-    return info.insertId
-}
 
 // Usuário
 
@@ -25,18 +15,4 @@ export async function validarUsuario(usuario) {
     let resposta = await con.query(comando, [usuario.nome, usuario.senha])
     let registro = resposta[0]
     return registro[0]
-}
-
-// Nome Usuário
-
-export async function consultaNomeUsuario(id) {
-    let comando = `
-    select  nome
-      from  tb_usuarios
-     where  id_usuario  = ?;
-    `
-
-    let resposta = await con.query(comando, [id])
-    let registro = resposta[0]
-    return registro
 }

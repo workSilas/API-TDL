58f9d1d725e20ac594354f3c9887b51462aaa7a1
bruhsvicente <brutronica931@gmail.com>
2024-11-01 17:36:33 -0300

    .

--- src/router.js
+++ src/router.js
@@ -1,8 +1,8 @@
 import usuarios from "./Controller/tb_usuariosController.js"
 import produtos from "./Controller/tb_produtosController.js"
 import vendas from "./Controller/tb_vendasController.js"
-import favoritos from ".//Controller/tb_favoritosController.js"
-import encomendas from ".//Controller/tb_encomendasController.js"
+import favoritos from "./Controller/tb_favoritosController.js"
+import encomendas from "./Controller/tb_encomendasController.js"
 
 export default function criarRotas(servidor) {
     servidor.use(usuarios)
